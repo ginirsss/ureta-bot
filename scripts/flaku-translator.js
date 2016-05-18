19e03a79efa3b74ccc2f7b192a7c769164e2175a
Gino Farías Milla <ginirsss@icloud.com>
2016-05-18 10:34:22 -0400

    otra flaku

--- scripts/flaku-translator.js
+++ scripts/flaku-translator.js
@@ -13,7 +13,7 @@
 // Author:
 //   @jorgeepunan
 
-var garabatus = ['wena care pico','sapoculiao','cómete un pan con pico','chúpalo','sale conchetumare','chupaelpico','tesapoelchupo','carepico','pico','maracaculia','con la punta del pico','esperenme, voy a cambiarme la toallita','micropene','queri un pan con tula CTM','fina la wea tiene hocico de pirata','se me calento el hocico','mande a la mierda al viejo culiao'];
+var garabatus = ['se me paro el pico','wena care pico','sapoculiao','cómete un pan con pico','chúpalo','sale conchetumare','chupaelpico','tesapoelchupo','carepico','pico','maracaculia','con la punta del pico','esperenme, voy a cambiarme la toallita','micropene','queri un pan con tula CTM','fina la wea tiene hocico de pirata','se me calento el hocico','mande a la mierda al viejo culiao'];
 
 function flakuTranslator (string) {
 
